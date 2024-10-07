fef9b05d0b47659535a06cbc549a84b706b83b62
Masin271 <masinmasin271@gmail.com>
2024-10-07 03:46:48 +0330

    Damn I'm stuck

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const body = document.querySelector("body");
+body.style.backgroundColor = "gray";
 const container = document.createElement("div");
 const button = document.createElement("button");
-container.style.maxWidth = "1200px";
-container.style.margin = "0 auto";
-container.style.backgroundColor = "black";
+container.classList.toggle("container");
+button.style.marginBottom = "10px";
+button.textContent = "grid size";
 body.appendChild(button);
 body.appendChild(container);
 function changeBackgroundColor() {
     this.style.backgroundColor = "black";
 }
-function gridFlex() {
-    for (let i = 0; i < 16; i++) {
+function gridFlex(a) {
+    for (let i = 0; i < a; i++) {
         let row = document.createElement("div");
         row.classList.toggle("row");
         row.classList.toggle("hover");
-            for (let j = 0; j < 16; j++) {
+            for (let j = 0; j < a; j++) {
                 let column = document.createElement("div");
                 column.classList.toggle("column");
                 column.classList.toggle("hover");
@@ -27,4 +28,7 @@ function gridFlex() {
     hover.addEventListener("mouseover",changeBackgroundColor)
 });
 }
-gridFlex();
+button.addEventListener("click", () =>{
+    let a = prompt("grid size");
+    gridFlex(a);
+});
